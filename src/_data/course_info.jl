80d1df064c26f0cd8ae2c934080735869148e6a5
Valentin Churavy <v.churavy@gmail.com>
2025-03-07 09:23:33 +0100

    fill in some details

--- src/_data/course_info.jl
+++ src/_data/course_info.jl
@@ -2,12 +2,11 @@ Dict(
     "course_name" => "Name of your course",
     "course_subtitle" => "a short catchy phrase about your course",
     "code" => "code of your course",
-    "semester" => "Fall 20XX",
+    "semester" => "2025",
     "authors" => [
-        "First author name" => "first author homepage link",
-        "Second author name" => "second author homepage link"
+        "Valentin Churavy" => "https://vchuravy.dev",
     ],
-    "institution" => "Your university (or similar) name",
+    "institution" => "University of Mainz",
     "institution_logo" => "name of your institution logo file, e.g. `logo.svg`. Place this under `assets`",
-    "repo" => "link to the repository of the source code"
+    "repo" => "https://github.com/vchuravy/rse-course"
 )
