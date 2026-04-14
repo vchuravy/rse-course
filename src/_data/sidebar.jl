3f1aa759d213624569a6486bf47b0fa501db4fb4
Valentin Churavy <v.churavy@gmail.com>
2026-04-14 17:30:11 +0200

    delete modules for now

--- src/_data/sidebar.jl
+++ src/_data/sidebar.jl
@@ -1,8 +1,4 @@
 [
     "welcome" => "Welcome",
     "module1" => "Module 1: Introduction",
-    "module2" => "Module 2: Principles of Research Software Engineering",
-    "module3" => "Module 3: Parallelism",
-    "module4" => "Module 4: Automatic Differentiation",
-    "module5" => "Module 5: Performance Engineering",
 ]
