98c10dc0d0983d79b7d77a3e7e24e40290ef5a7c
Valentin Churavy <v.churavy@gmail.com>
2026-05-19 15:48:39 +0200

    Handle upcoming lectures nicer (#35)

--- src/assets/scripts/get_schedule.jl
+++ src/assets/scripts/get_schedule.jl
@@ -1,4 +1,8 @@
+import Dates
+
 let
+    today = Dates.today()
+
     sections = metadata["sidebar"]
     sections = [
         @htl("""
@@ -6,20 +10,29 @@ let
             let
                 input = other_page.input
                 output = other_page.output
-                
+
                 name = get(output.frontmatter, "title", basename(input.relative_path))
                 desc = get(output.frontmatter, "description", nothing)
                 tags = get(output.frontmatter, "tags", String[])
-                date = get(output.frontmatter, "date", nothing)
+                date_str = get(output.frontmatter, "date", nothing)
 
-                class = [
-                    "no-decoration",
-                    ("tag_$(replace(x, " "=>"_"))" for x in tags)...,
-                ]
-                if date !== nothing
+                if date_str !== nothing
+                    upcoming = try
+                        Dates.Date(string(date_str)) > today
+                    catch
+                        false
+                    end
+
+                    class = [
+                        "no-decoration",
+                        upcoming ? "upcoming-entry" : nothing,
+                        ("tag_$(replace(x, " "=>"_"))" for x in tags)...,
+                    ]
+
                     @htl("""<a title=$(desc) class=$(class) href=$(root_url * "/" * other_page.url)>
                         <h3>$(name)</h3>
-                        $(date)
+                        <span class="schedule-date $(upcoming ? "upcoming-badge" : "")">$(date_str)</span>
+                        $(upcoming ? @htl("""<span class="upcoming-label">Upcoming</span>""") : nothing)
                     </a>""")
                 else
                     nothing
